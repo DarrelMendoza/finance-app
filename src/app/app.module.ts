78349c762e4dd015c0c5c8ae992fc3834f3aeeca
Darrel Mendoza <darrelmendoza85@gmail.com>
2023-08-13 00:30:07 +0800

    feat(expense-tracker): created expense tracker page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule
